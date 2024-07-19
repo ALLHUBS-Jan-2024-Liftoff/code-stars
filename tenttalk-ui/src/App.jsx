703ed2fb1e1195d492ef6cba7e74198f9ad83ab0
mads-bohn <mady.bohn@gmail.com>
2024-07-18 22:33:10 -0500

    completed navbar

--- tenttalk-ui/src/App.jsx
+++ tenttalk-ui/src/App.jsx
@@ -4,6 +4,9 @@ import { Index } from './pages/index'
 import { ReviewForm } from './pages/reviewform'
 import { Campground } from './pages/campground'
 import { Search } from './pages/search'
+import { Login } from './pages/login'
+import { Register } from './pages/register'
+import { Account } from './pages/account'
 
 
 function App() {
@@ -15,6 +18,9 @@ function App() {
         <Route path="/review" element={<ReviewForm/>} />
         <Route path="/campground" element={<Campground/>} />
         <Route path="/search" element={<Search/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/account" element={<Account/>} />
       </Routes>
     </HashRouter>
   )
