95eb1d9381baadf54a833fab395823a7811f563c
mads-bohn <mady.bohn@gmail.com>
2024-07-22 14:59:32 -0500

    login and register forms

--- tenttalk-ui/src/pages/register.jsx
+++ tenttalk-ui/src/pages/register.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from "../components/navbar";
+import RegisterForm from '../components/account/RegisterForm';
 
 export function Register() {
   return (
     <div>
       <Navbar />
       <h1>Register</h1>
+      <RegisterForm />
     </div>
   )
 }
