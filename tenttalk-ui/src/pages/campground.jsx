3d226062495b415dff0c20b23734cfe4f57d8733
Amanda Darby <you@example.com>
2024-07-21 19:28:17 -0500

    SimpleShareButtonCampground

--- tenttalk-ui/src/pages/campground.jsx
+++ tenttalk-ui/src/pages/campground.jsx
@@ -1,11 +1,24 @@
 import Navbar from "../components/navbar";
 
+import React from 'react';
+
+import Share from '../components/share';
+
+
+
 export function Campground() {
     return(
     <div>
         <Navbar />
         <h1>Campground Page</h1>
         <p>Hello, Tent Talk!</p> 
+
+
+        <Share />
+
+
     </div>
     );
+
+    
 }
