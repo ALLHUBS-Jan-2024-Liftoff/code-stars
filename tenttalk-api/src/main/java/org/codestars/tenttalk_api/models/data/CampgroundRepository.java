bcda69c5e33e2cadfc40c1740d1d6a922e624cb2
2amkoding <2amkoding@gmail.com>
2024-07-22 16:04:11 -0500

    Moved Data under Models, updated application.properties, Added annotations

--- tenttalk-api/src/main/java/org/codestars/tenttalk_api/models/data/CampgroundRepository.java
+++ tenttalk-api/src/main/java/org/codestars/tenttalk_api/models/data/CampgroundRepository.java
@@ -1,7 +1,6 @@
-package data;
+package org.codestars.tenttalk_api.models.data;
 
-import models.Campground;
-import models.User;
+import org.codestars.tenttalk_api.models.Campground;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
