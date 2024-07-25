67c2d425cc22de68409093a252ca8939f3b308fb
mads-bohn <mady.bohn@gmail.com>
2024-07-24 22:15:24 -0500

    use effect

--- tenttalk-ui/src/components/search/ResultTable.jsx
+++ tenttalk-ui/src/components/search/ResultTable.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function ResultTable() {
   
     const [campgrounds, setCampgrounds] = useState([]);
-    
+    useEffect(() => {
+        
+    });
   
     return (
     <div>
