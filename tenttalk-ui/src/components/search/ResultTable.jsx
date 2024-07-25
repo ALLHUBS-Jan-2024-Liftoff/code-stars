d36b30efcf4fa05d27f67e7af13584f0383ee22e
mads-bohn <mady.bohn@gmail.com>
2024-07-24 21:17:04 -0500

    table layout

---
+++ tenttalk-ui/src/components/search/ResultTable.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function ResultTable() {
+  
+    const [campgrounds, setCampgrounds] = useState([]);
+    
+  
+    return (
+    <div>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th scope="col">Campground Name</th>
+                    <th scope="col">Rating</th>
+                    <th scope="col">Button</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Example campground</td>
+                    <td>5</td>
+                    <td>Button</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  )
+}
