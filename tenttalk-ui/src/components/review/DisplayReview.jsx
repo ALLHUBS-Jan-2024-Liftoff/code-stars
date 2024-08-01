86b69331c8496e89dd677b6575b3cc12d80635d2
mads-bohn <mady.bohn@gmail.com>
2024-07-31 22:57:45 -0500

    added review info to DisplayReview via axios and useEffect

--- tenttalk-ui/src/components/review/DisplayReview.jsx
+++ tenttalk-ui/src/components/review/DisplayReview.jsx
@@ -1,16 +1,26 @@
 import React, { useState } from 'react'
+import { useEffect } from 'react';
+import axios from 'axios';
 
 export default function DisplayReview({id}) {
 
   const [review, setReview] = useState([]);
 
-  const loadCampgrounds = async () => {
-    const result = await axios.get(`http://localhost:8080/review/${reviewId}`);
+  //loads review when page is loaded
+  useEffect(() => {
+    loadReview();
+},[]);
+
+  const loadReview = async () => {
+    const result = await axios.get(`http://localhost:8080/review/${id}`);
+    console.log(result.data);
     setReview(result.data); 
 }
   return (
     <div>
-      
+      <h2>Honest Review</h2>
+      <p>Rating: {review.rating} stars</p>
+      <p>"{review.feedback}"</p>
     </div>
   )
 }
