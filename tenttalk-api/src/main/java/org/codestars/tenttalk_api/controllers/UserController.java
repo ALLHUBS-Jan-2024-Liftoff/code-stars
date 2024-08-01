0062467c985920dd7d06b1fafc1e7a1bf6ee42fb
mads-bohn <mady.bohn@gmail.com>
2024-08-01 18:59:02 -0500

    added cross origin annotation

--- tenttalk-api/src/main/java/org/codestars/tenttalk_api/controllers/UserController.java
+++ tenttalk-api/src/main/java/org/codestars/tenttalk_api/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
     private final UserService userService;
 
