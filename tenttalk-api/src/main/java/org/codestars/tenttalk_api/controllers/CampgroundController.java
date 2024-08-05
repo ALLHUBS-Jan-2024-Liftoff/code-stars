449aa38d394215dd504ea936d8b5a098fcd464d7
2amkoding <2amkoding@gmail.com>
2024-08-04 20:46:30 -0500

    campgroundController add (origins = http://localhost:5173)

--- tenttalk-api/src/main/java/org/codestars/tenttalk_api/controllers/CampgroundController.java
+++ tenttalk-api/src/main/java/org/codestars/tenttalk_api/controllers/CampgroundController.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/campground")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:5173")
 public class CampgroundController {
 
     @Autowired
