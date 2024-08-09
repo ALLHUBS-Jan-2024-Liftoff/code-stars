8f71026dfc167c26211e4750a7571a2b2194a1d8
2amkoding <2amkoding@gmail.com>
2024-08-09 18:50:20 -0500

    created Favorite Class

--- tenttalk-api/src/main/java/org/codestars/tenttalk_api/models/Favorite.java
+++ tenttalk-api/src/main/java/org/codestars/tenttalk_api/models/Favorite.java
@@ -1,1 +1,38 @@
+package org.codestars.tenttalk_api.models;
 
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Favorite extends AbstractEntity {
+
+private Campground campground;
+
+private User user;
+
+    public Favorite(Campground campground, User user) {
+        this.campground = campground;
+        this.user = user;
+    }
+
+    public Favorite(){}
+
+    public Campground getCampground() {
+        return campground;
+    }
+
+    public void setCampground(Campground campground) {
+        this.campground = campground;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    //spent 8.5 hrs trying to make favorites abstract and failed t(-_-)t
+    //Doing it the boring way.
+}
--- tenttalk-api/src/main/java/org/codestars/tenttalk_api/service/FavoriteService.java
+++ tenttalk-api/src/main/java/org/codestars/tenttalk_api/service/FavoriteService.java
@@ -1,4 +1,7 @@
 package org.codestars.tenttalk_api.service;
 
 public interface FavoriteService {
+
+
+
 }
