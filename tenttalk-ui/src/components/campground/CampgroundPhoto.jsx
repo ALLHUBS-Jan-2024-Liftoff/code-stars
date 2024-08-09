f58a0f78f37d780f82eb785bb7135982f644da09
mads-bohn <mady.bohn@gmail.com>
2024-08-09 16:54:29 -0500

    axios post request working, response keyword not working

--- tenttalk-ui/src/components/campground/CampgroundPhoto.jsx
+++ tenttalk-ui/src/components/campground/CampgroundPhoto.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import axios from 'axios'
+import { useEffect } from 'react';
 
-export default function CampgroundPhoto() {
-    
+export default function CampgroundPhoto({campgroundName}) {
+
+  useEffect(() => {
+    loadPhoto();
+  },[]);
+
+  async function loadPhoto() {
+    const postRequestHeaders = {
+        "X-Goog-Api-Key": "{key}",
+        "X-Goog-FieldMask": "places.photos"
+    }
+
+    const postRequestData = {
+      textQuery: `${campgroundName}`
+    }
+
+    await axios.post("https://places.googleapis.com/v1/places:searchText", postRequestData, {
+      headers : postRequestHeaders
+    });
+    console.log(response.data);
+
+  }
 
   return (
     <div>CampgroundPhoto</div>
--- tenttalk-ui/src/pages/campground.jsx
+++ tenttalk-ui/src/pages/campground.jsx
@@ -7,6 +7,7 @@ import { DisplayRating } from "../components/review/DisplayRating";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import CampgroundPhoto from "../components/campground/CampgroundPhoto";
 
 
 
@@ -33,6 +34,7 @@ export function Campground() {
     <div>
         <Navbar />
         <ViewCampground />
+        <CampgroundPhoto campgroundName={"Gateway Arch"}/>
         <div>
             <h3>Average Rating: {(Math.round(campground.rating*10))/10 }</h3>
             <DisplayRating rating={ campground.rating } size={36} />
