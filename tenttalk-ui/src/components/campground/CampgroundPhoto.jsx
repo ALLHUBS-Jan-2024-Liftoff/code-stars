00c803cfc7594253cf4e0ea3017d6f542df854ca
mads-bohn <mady.bohn@gmail.com>
2024-08-09 23:10:14 -0500

    post request successful, created env file

--- tenttalk-ui/src/components/campground/CampgroundPhoto.jsx
+++ tenttalk-ui/src/components/campground/CampgroundPhoto.jsx
@@ -2,26 +2,42 @@ import React from 'react'
 import axios from 'axios'
 import { useEffect } from 'react';
 
-export default function CampgroundPhoto({campgroundName}) {
+export default function CampgroundPhoto({searchQuery}) {
 
   useEffect(() => {
-    loadPhoto();
+    getPhoto();
   },[]);
 
-  async function loadPhoto() {
-    const postRequestHeaders = {
-        "X-Goog-Api-Key": "{key}",
+  async function getPhoto() {
+
+    //creates new instance of Request with url, method, headers, and body
+    const postRequest = new Request("https://places.googleapis.com/v1/places:searchText", {
+      method: "POST",
+      headers: {
+        "X-Goog-Api-Key": "api key here",
         "X-Goog-FieldMask": "places.photos"
+      },
+      body: JSON.stringify({textQuery: `${searchQuery}`})
+    })
+
+    let photoName = "";
+
+
+    try {
+      // fetches postResponse and reads in json format
+      const postResponse = await fetch(postRequest);
+      const postJson = await postResponse.json();
+
+      console.log(postJson.places[0].photos[0]);
+      if (!postResponse.ok) {
+        throw new Error(`Response status: ${postResponse.status}`);
+      } 
+    } catch (error) {
+      console.error(error.message);
     }
 
-    const postRequestData = {
-      textQuery: `${campgroundName}`
-    }
 
-    await axios.post("https://places.googleapis.com/v1/places:searchText", postRequestData, {
-      headers : postRequestHeaders
-    });
-    console.log(response.data);
+    
 
   }
 
