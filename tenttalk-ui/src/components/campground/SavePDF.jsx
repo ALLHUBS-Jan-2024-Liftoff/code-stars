7d7977861f9f80434d6879133431b8552f3b04bc
mads-bohn <mady.bohn@gmail.com>
2024-08-12 19:05:11 -0500

    installed react-to-pdf

---
+++ tenttalk-ui/src/components/campground/SavePDF.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useRef } from 'react'
+import generatePDF from 'react-to-pdf'
+
+export default function SavePDF() {
+    const targetRef = useRef();
+
+
+    return (
+        <div>SavePDF</div>
+    )
+}
